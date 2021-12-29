8145e74581d9793878819971f81c1b2cd3bb27a1
Ivon <ivonne@jjf.com>
2021-12-29 11:44:06 -0600

    add service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+
+const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
 // const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 // const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require('path');
+
 
 const config = {
   entry: {
